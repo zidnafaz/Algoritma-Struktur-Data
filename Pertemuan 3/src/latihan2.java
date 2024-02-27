46afee0cefc00bc80482c0769011d91a997743a1
zidnafaz <zidnafaza27@gmail.com>
2024-02-27 11:08:39 +0700

    Latihan Soal

---
+++ Pertemuan 3/src/latihan2.java
@@ -0,0 +1,15 @@
+public class latihan2 {
+    public static void main(String[] args) {
+        int[] daftarNilai = new int[5];
+
+        daftarNilai[0] = 7;
+        daftarNilai[1] = 3;
+        daftarNilai[2] = 8;
+        daftarNilai[3] = 2;
+        daftarNilai[4] = 1;
+
+        for (int i : daftarNilai) {
+            System.out.println(i);
+        }
+    }
+}
