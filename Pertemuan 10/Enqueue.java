09b803e0c4118bc37952a5f21341a97969584266
zidnafaz <zidnafaza27@gmail.com>
2024-04-23 10:38:53 +0700

    Pertemuan 10

---
+++ Pertemuan 10/Enqueue.java
@@ -0,0 +1,20 @@
+public class Enqueue {
+
+    public static void main(String[] args) {
+        if (IsFull()) {
+            System.out.println("Queue sudah penuh");
+        } else {
+            if (IsEmpty()) {
+                front = rear = 0;
+            } else {
+                if (rear == max -1) {
+                    rear = 0;
+                } else {
+                    rear++;
+                }
+            }
+            Q[rear] = data;
+            size++;
+        }
+    }
+}
